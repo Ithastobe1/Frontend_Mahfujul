57f98a8fdf724e9042a6383176d5b46799e3ea73
unknown <shajibhasan7325@gmail.com>
2023-03-04 22:34:28 +0600

    fist commit

---
+++ components/Earn/index.js
@@ -0,0 +1,18 @@
+import Image from 'next/image';
+import React from 'react';
+import Logo from '../../assets/img/Rectangle 999.png';
+
+const Index = () => {
+  return (
+    <div className="testimonial ">
+      <Image
+        src={Logo.src}
+        width={1080}
+        height={214}
+        style={{ objectFit: 'cover' }}
+      />
+    </div>
+  );
+};
+
+export default Index;
